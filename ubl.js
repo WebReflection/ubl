e2754cdc07e1ea220294d84225547957954daf34
Andrea Giammarchi <a.giammarchi@eyeo.com>
2019-02-06 05:26:30 +0100

    removed useless defer within the ubl JS code

--- ubl.js
+++ ubl.js
@@ -1,4 +1,4 @@
 (function(g,d,s){if(!g.ESM){
 s=d.documentElement;s=s.insertBefore(d.createElement('script'),s.lastChild);
-s.defer=!0;s.type='text/javascript';s.src=g.Reflect?'es2015.js':'es5.js';
+s.type='text/javascript';s.src=g.Reflect?'es2015.js':'es5.js';
 }}(window,document));
